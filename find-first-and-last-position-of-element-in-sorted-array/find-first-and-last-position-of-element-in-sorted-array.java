2c52afaba93ad307ad44c9d8bef5710f50c75b02
Shashwath Kamath <kamathshashwath1991@gmail.com>
2020-12-26 15:49:38 -0500

    Time: 1 ms (14.98%), Space: 42.3 MB (47.14%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,49 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
+        if (nums.length == 0 || nums == null) return new int[]{-1,-1};
         int first = binarySearchFirst(nums,target);
-        if (first == -1){
-            return new int[]{-1,-1};
-        }
-        int last = binarySearchLast(nums,target);
-        return new int[]{first,last};
+        System.out.println(first);
+        if (first < 0) return new int[]{-1,-1};
+        int second = binarySearchSecond(nums,target);
+        return new int[]{first,second};
     }
     
-    private int binarySearchFirst(int[] nums,int target){
-        int left = 0, right = nums.length-1;
-        int index =-1;
-        
+    private int binarySearchSecond(int[] nums,int target){
+        int index = -1, left = 0, right = nums.length-1;
         while(left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] >= target){
-                if (nums[mid] == target){
-                    index = mid;
-                }
-                right = mid -1;
-            }
-            else{
-                left = mid + 1;
-            }
-        }
-        
-        return index;
-    }
-    
-    private int binarySearchLast(int[] nums,int target){
-        int left = 0, right = nums.length-1;
-        int index = -1;
-        
-        while(left <= right){
-            int mid = left + (right - left)/2;
+            int mid = left+ (right - left)/2;
             if (nums[mid] <= target){
                 if (nums[mid] == target){
                     index = mid;
                 }
-                left = mid + 1;
+                left = mid+1;
             }
             else{
-                right = mid - 1;
+                right = mid-1;
             }
         }
+    
+    return index;
+    }
+    
+    
+    
+    private int binarySearchFirst(int[] nums,int target){
         
+        int index = -1, left = 0, right = nums.length-1;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if (nums[mid] >= target){
+                if (nums[mid] == target){
+                    index = mid;
+                }
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
         return index;
     }
 }
