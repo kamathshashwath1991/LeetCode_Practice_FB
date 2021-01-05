b4151793c81e00a82e396e9e9c78a6ab5c6f9c95
Shashwath Kamath <kamathshashwath1991@gmail.com>
2021-01-05 14:19:04 -0500

    Time: 0 ms (100.00%), Space: 36.4 MB (42.33%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,20 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        int amount[] = new int[nums.length];
-        
-        amount[0] = nums[0];
-        if (nums.length == 1){
-            return amount[0];
+        if (nums.length == 0 || nums == null) return 0;
+        int skip = 0;
+        int take = nums[0];
+        for (int i= 1; i < nums.length; i++){
+            int temp = skip;
+            skip = Math.max(skip,take);
+            take = temp + nums[i];
         }
-        // as we can get amount from any index other than one index less, so we have
-        //to compare first two houses to determine the path.
-        amount[1] = Math.max(amount[0],nums[1]);
-        
-        for (int i = 2; i < nums.length; i++){
-            amount[i] = Math.max(amount[i-2] + nums[i],amount[i-1]);
-        }
-        
-        return amount[nums.length-1];
+    return Math.max(skip,take);
     }
+       
 }
