56a571b0552ab2de96773fdf1dadb39bd453518b
Shashwath Kamath <kamathshashwath1991@gmail.com>
2021-01-13 20:57:10 -0500

    Time: 1 ms (96.17%), Space: 39.2 MB (45.63%) - LeetHub

--- construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -16,30 +16,21 @@
 class Solution {
     HashMap<Integer,Integer> map;
     int index;
-    public TreeNode buildTree(int[] inorder, int[] postorder){
-            if(postorder.length == 0){
-                return null;
-            }
-        int n = postorder.length;
-        this.map = new HashMap<>();
-        this.index = n-1;
-            //add all elements of inorder to hashmap
-        for(int i =0 ;i < inorder.length; i++){
-            map.put(inorder[i] , i);
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        map = new HashMap<>();
+        index = postorder.length-1;
+        for (int i = 0; i < inorder.length; i++){
+            map.put(inorder[i],i);
         }
-       return helper(inorder,postorder,0,inorder.length-1);
+        return helper(inorder,postorder,0,inorder.length-1);
     }
-    private TreeNode helper(int[] inorder,int[]postorder,int start,int end){
-        //base
-        if(start > end){
-            return null;
-        }
-        
-        TreeNode root = new TreeNode(postorder[index--]);
-        int rootIndex = map.get(root.val);
-        root.right = helper(inorder,postorder, rootIndex+1, end);
-        root.left = helper(inorder,postorder, start , rootIndex - 1);
-        
+    
+    private TreeNode helper(int[] inorder, int[] postorder,int start, int end){
+        if (start > end) return null;
+        TreeNode root = new TreeNode(postorder[index]);
+        index--;
+        root.right = helper(inorder,postorder,map.get(root.val)+1,end);
+        root.left = helper(inorder,postorder,start,map.get(root.val)-1);
         return root;
     }
 }
