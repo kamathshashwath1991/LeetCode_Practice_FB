34ca8e58f768e3f81e309da78c0e8f276bb28c77
Shashwath Kamath <kamathshashwath1991@gmail.com>
2021-01-17 23:54:06 -0500

    Time: 478 ms (16.74%), Space: 43.5 MB (37.09%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,16 +1,11 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-         List<List<Integer>> list = new ArrayList<>();
-        if (nums == null || nums.length == 0){
-            return list;
-        }
-        
+      
         Arrays.sort(nums);
-        //[-4,-1,-1,0,1,2]
-        
         HashSet<List<Integer>> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++){
-            int j = i + 1;
+        
+        for (int i = 0; i < nums.length; i++){
+            int j = i+1;
             int k = nums.length-1;
             while(j < k){
                 int sum = nums[j] + nums[k];
@@ -19,7 +14,7 @@ class Solution {
                     j++;
                     k--;
                 }
-                else if(sum > -nums[i]){
+                else if (sum > -nums[i]){
                     k--;
                 }
                 else{
@@ -27,6 +22,8 @@ class Solution {
                 }
             }
         }
+        
         return new ArrayList<>(set);
+        
     }
 }
