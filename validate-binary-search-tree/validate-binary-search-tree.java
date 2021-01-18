995ab226c35522de9baab481b1e96366a64003a1
Shashwath Kamath <kamathshashwath1991@gmail.com>
2021-01-18 16:28:14 -0500

    Time: 0 ms (100.00%), Space: 39 MB (29.80%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.java
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,19 +15,17 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        
-        if (root == null ) return true;
-        
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return helper(root,Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
     private boolean helper(TreeNode root, long min, long max){
-        if (root == null){
-            return true;
+        if (root == null) return true;
+        
+        if (root.val <= min || root.val >= max){
+                return false;
         }
-        if (root.val > min && root.val < max){
-            return helper(root.left,min,root.val) && helper(root.right,root.val,max);
-        }
-     return false;
+        
+        
+        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
     }
 }
