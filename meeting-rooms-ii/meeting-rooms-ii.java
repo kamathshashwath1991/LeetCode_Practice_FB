467575d9f359f5a08c7bccc1a711db1d59aad885
Shashwath Kamath <kamathshashwath1991@gmail.com>
2021-02-07 08:18:55 -0500

    Time: 6 ms (77.28%), Space: 39.2 MB (38.60%) - LeetHub

---
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, (a,b) -> a[0]- b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int[] interval : intervals){
+            if (pq.size() > 0 && pq.peek() <= interval[0]){
+                pq.poll();
+            }
+            pq.offer(interval[1]);
+        }
+        
+        return pq.size();
+    }
+}
